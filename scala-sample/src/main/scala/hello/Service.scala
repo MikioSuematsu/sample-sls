5f32c43206bb96294172866cbbb6dd44999474bc
A13626 <suematsu_mikio@cyberagent.co.jp>
2017-06-27 21:18:19 +0900

    名前変更

--- scala-sample/src/main/scala/hello/Service.scala
+++ scala-sample/src/main/scala/hello/Service.scala
@@ -5,13 +5,13 @@ import com.amazonaws.services.lambda.runtime.Context
 //Sserverless.ymlで呼び出すクラス
 // lambdaと1対1の関係のため、ハンドラではなく直接サービスとして扱う
 // このサンプルの例では、Cake Pattern用いてテストで依存性の注入を行っている
-class Service extends ServiceImpl with MixInSlackRepository
+class Service extends ServiceInterface with MixInSlackRepository
 
-trait ServiceImpl extends UseSlackRepository {
+trait ServiceInterface extends UseSlackRepository {
 	//メソッド名やクラスは何でも良い
 	def get(input: HelloRequest, context: Context): Response = {
 		val message = s" query: ${input.getQueryStringParameters}"
 		slack.post(message) //実際の業務処理
-		return SucessResponse.apply(message)
+		SucessResponse.apply(message)
 	}
 }
